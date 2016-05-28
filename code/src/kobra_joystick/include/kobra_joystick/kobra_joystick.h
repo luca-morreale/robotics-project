77e53cb8695bd0b69f0cdcc6696e7075a66e4f5d
Luca Morreale <luca.morreale93@gmail.com>
2016-05-29 01:03:50 +0200

    Created kobra joystick node
    Created kobra joystick node, NOT TESTED

---
+++ code/src/kobra_joystick/include/kobra_joystick/kobra_joystick.h
@@ -0,0 +1,76 @@
+#include <map>
+
+#include <termios.h>
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+
+namespace gazebo {
+
+    #define KEYBOARD 0
+    #define PAN      "pan"
+    #define TILT     "tilt"
+    #define LINEAR   "linear"
+    #define ANGULAR  "angular"
+
+    #define RIGHT_ARROW  0x43 
+    #define LEFT_ARROW   0x44
+    #define UP_ARROW     0x41
+    #define DOWN_ARROW   0x42
+
+    #define TILT_UP      0x11
+    #define TILT_DOWN    0x1f
+    #define PAN_LEFT     0x1e
+    #define PAN_RIGHT    0x20
+
+    #define FZERO 0.0
+    #define DEFAULT_SCALE 2.0
+
+
+    typedef struct Command {
+
+        double kobra_omega = FZERO;
+        double kobra_linear = FZERO;
+        double camera_pan = FZERO;
+        double camera_tilt = FZERO;
+        bool validity = true;
+
+        bool isPanTilt() {
+            return validity && (camera_pan > 0 || camera_tilt > 0);
+        }
+        
+        bool isKobra() {
+            return validity && (kobra_omega > 0 || kobra_linear > 0);
+        }
+
+    } Command;
+
+    class KeyboardReader {
+    public:
+        KeyboardReader();
+        ~KeyboardReader();
+
+        void keyLoop();
+
+    protected:
+        virtual Command *transformCommand(char cmd);
+        virtual void initScaleFactors();
+
+    private:
+        ros::NodeHandle *rosnode;
+        ros::Publisher velocity_pub;
+        ros::Publisher pantilt_pub;
+
+        double l_scale, w_scale;
+        double p_scale, t_scale;
+
+        std::map<std::string, double> velocities;
+
+        struct termios old_settings;
+        struct termios raw_settings;
+
+        void getRawKeyboard();
+    };
+}
+
---
+++ code/src/kobra_joystick/src/kobra_joystick.cpp
@@ -0,0 +1,154 @@
+#include "kobra_joystick/kobra_joystick.h"
+
+#include <iostream>
+
+using namespace gazebo;
+
+KeyboardReader::KeyboardReader()
+{
+        rosnode = new ros::NodeHandle();
+
+        initScaleFactors();
+
+        velocity_pub = rosnode->advertise<geometry_msgs::Twist>("kobra/cmd_vel", 1);
+        pantilt_pub = rosnode->advertise<geometry_msgs::Vector3>("kobra/camera_mv", 1);
+}
+
+void KeyboardReader::initScaleFactors()
+{
+    rosnode->param("linear_scale", l_scale, DEFAULT_SCALE);
+    rosnode->param("omega_scale", w_scale, DEFAULT_SCALE);
+    rosnode->param("pan_scale", p_scale, DEFAULT_SCALE);
+    rosnode->param("tilt_scale", t_scale, DEFAULT_SCALE);
+
+    rosnode->param("linear_vel", velocities[LINEAR], DEFAULT_SCALE);
+    rosnode->param("angular_vel", velocities[ANGULAR], DEFAULT_SCALE);
+    rosnode->param("pan_degree", velocities[PAN], DEFAULT_SCALE);
+    rosnode->param("tilt_degree", velocities[TILT], DEFAULT_SCALE);
+}
+
+void KeyboardReader::keyLoop()
+{
+    getRawKeyboard();
+
+    ROS_INFO("Reading commands from your keyboard");
+    ROS_INFO("---------------------------");
+    ROS_INFO("Use A and D to pan the camera, use W and S to tilt the camera.");
+    ROS_INFO("---------------------------");
+    ROS_INFO("Use arrow keys to move the robot.");
+
+    char c;
+
+    while(ros::ok()) {
+
+        if(read(KEYBOARD, &c, 1) < 0) {
+            std::cerr << "read() error: " << c << std::endl;
+            exit(-1);
+        }
+      
+        Command *cmd = transformCommand(c);
+     
+        geometry_msgs::Twist twist_cmd;
+        twist_cmd.angular.z = w_scale * cmd->kobra_omega;
+        twist_cmd.linear.x = l_scale * cmd->kobra_linear;
+
+        geometry_msgs::Vector3 pantilt_cmd;
+        pantilt_cmd.x = p_scale * cmd->camera_pan;
+        pantilt_cmd.y = t_scale * cmd->camera_tilt;
+
+        if(cmd->isKobra()) {
+            velocity_pub.publish(twist_cmd);
+            
+        } else if(cmd->isPanTilt()) {
+            pantilt_pub.publish(pantilt_cmd);
+        }
+    }
+
+    return;
+}
+
+// DEBUG TO BE REMOVED AFTER TESTING
+Command *KeyboardReader::transformCommand(char c)
+{
+    Command *cmd = (Command *) malloc(sizeof(Command));
+    
+    switch(c) {
+        case LEFT_ARROW:
+            ROS_DEBUG("MOVE LEFT");
+            cmd->kobra_omega = velocities[ANGULAR];
+            break;
+        case RIGHT_ARROW:
+            ROS_DEBUG("MOVE RIGHT");
+            cmd->kobra_omega = -velocities[ANGULAR];
+            break;
+        case UP_ARROW:
+            ROS_DEBUG("MOVE FORWARD");
+            cmd->kobra_linear = velocities[LINEAR];
+            break;
+        case DOWN_ARROW:
+            ROS_DEBUG("MOVE BACKWARD");
+            cmd->kobra_linear = -velocities[LINEAR];
+            break;
+        case TILT_UP:
+            ROS_DEBUG("TILT UP");
+            cmd->camera_tilt = velocities[TILT];
+            break;
+        case TILT_DOWN:
+            ROS_DEBUG("TILT DOWN");
+            cmd->camera_tilt = -velocities[TILT];
+            break;
+        case PAN_LEFT:
+            ROS_DEBUG("PAN LEFT");
+            cmd->camera_pan = velocities[PAN];
+            break;
+        case PAN_RIGHT:
+            ROS_DEBUG("PAN RIGHT");
+            cmd->camera_pan = -velocities[PAN];
+            break;
+        default:
+            cmd->validity = false;
+    }
+    return cmd;
+}
+
+void KeyboardReader::getRawKeyboard() 
+{
+    tcgetattr(KEYBOARD, &old_settings);                             /* Get the console settings                 */
+    memcpy(&raw_settings, &old_settings, sizeof(struct termios));   /* Clone keyboard settings                  */
+    raw_settings.c_lflag &= ~(ICANON | ECHO);                       /* Disable buffered i/o and set echo mode   */
+                        
+    raw_settings.c_cc[VEOL] = 1;                                    /* Set additional End-Of-Line character     */
+    raw_settings.c_cc[VEOF] = 2;                                    /* Sets End-Of-File special character       */
+
+    tcsetattr(KEYBOARD, TCSANOW, &raw_settings);                    /* Use these new terminal i/o settings      */
+}
+
+KeyboardReader::~KeyboardReader()
+{
+    tcsetattr(KEYBOARD, TCSANOW, &old_settings);
+}
+
+
+
+
+#include <signal.h>
+
+void quit(int sig);
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "kobra_keyboard");
+    KeyboardReader joystic;
+
+    signal(SIGINT, quit);
+    joystic.keyLoop();
+    
+    return 0;
+}
+
+void quit(int sig)
+{
+    (void)sig;
+    ros::shutdown();
+    exit(0);
+}
