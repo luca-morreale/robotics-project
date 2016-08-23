97753f194c4c71dec96b87d36e697042b9453e58
albertogaspar <gaspa@joaogas.it>
2016-08-23 11:05:14 +0200

    8 location and loop added

--- code/src/simple_navigation_goals/src/simple_navigation_goals.cpp
+++ code/src/simple_navigation_goals/src/simple_navigation_goals.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <limits.h>
+#include <unistd.h>
 #include "../include/simple_navigation_goals/strtk.hpp"
 
 using namespace std;
@@ -14,6 +16,13 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "simple_navigation_goals");
+
+	//Print directory where the exec is running from -- Used for debug purpose only
+	char result[ PATH_MAX ];
+	ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+	std::string path = std::string( result, (count > 0) ? count : 0 );
+	cout << path;
+
 
 	//tell the action client that we want to spin a thread by default
 	MoveBaseClient ac("move_base", true);
@@ -26,49 +35,50 @@ int main(int argc, char** argv){
 
 	move_base_msgs::MoveBaseGoal goal;
 
-	//we'll send a goal to the robot reading from the txt file
-	std::ifstream file("../../src/simple_navigation_goals/path.txt");
-	if(file.is_open()){
-		std::string str;
-
-		while(std::getline(file, str)){
-			std::vector<double> double_vector;
-			if(strtk::parse(str," ",double_vector)){
-				//Print values
-				cout<<"Received goal is: ";
-				for(int i=0; i<double_vector.size(); i++){
-					cout<<double_vector[i]<<" ";
+	while(true){
+		//we'll send a goal to the robot reading from the txt file
+		std::ifstream file("../../../src/simple_navigation_goals/path.txt");
+		if(file.is_open()){
+			std::string str;
+			while(std::getline(file, str)){
+				std::vector<double> double_vector;
+				if(strtk::parse(str," ",double_vector)){
+					//Print values
+					cout<<"Received goal is: ";
+					for(int i=0; i<double_vector.size(); i++){
+						cout<<double_vector[i]<<" ";
+					}
+					cout<<endl;
 				}
-				cout<<endl;
+				else{
+					ROS_ERROR("Cannot read from text file.");
+					file.close();
+					return -1;
+				}
+				goal.target_pose.header.frame_id = "map";
+				goal.target_pose.header.stamp = ros::Time::now();
+
+				goal.target_pose.pose.position.x = double_vector[0];
+				goal.target_pose.pose.position.y = double_vector[1];
+				goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(double_vector[3]);;
+
+				ROS_INFO("Sending goal");
+				ac.sendGoal(goal);
+
+				ac.waitForResult();
+
+				if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+					ROS_INFO("The goal has been reached, passing to the next one");
+				else
+					ROS_INFO("The robot failed to reach the goal for some reason");
 			}
-			else{
-				ROS_ERROR("Cannot read from text file.");
-				file.close();
-				return -1;
-			}
-			goal.target_pose.header.frame_id = "map";
-			goal.target_pose.header.stamp = ros::Time::now();
-
-			goal.target_pose.pose.position.x = double_vector[0];
-			goal.target_pose.pose.position.y = double_vector[1];
-			goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(double_vector[3]);;
-
-			ROS_INFO("Sending goal");
-			ac.sendGoal(goal);
-
-			ac.waitForResult();
-
-			if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-				ROS_INFO("The goal has been reached, passing to the next one");
-			else
-				ROS_INFO("The robot failed to reach the goal for some reason");
 		}
+		else{
+			ROS_WARN("Cannot open the file path.txt");
+			cerr << "Error: " << strerror(errno);
+			return -1;
+		}
+		file.close();
 	}
-	else{
-		ROS_WARN("Cannot open the file path.txt");
-		cerr << "Error: " << strerror(errno);
-		return -1;
-	}
-	file.close();
 	return 0;
 }
